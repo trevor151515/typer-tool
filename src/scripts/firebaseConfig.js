89d7a6bb68090b8d9014a9176426496119714d4c
trevor151515 <trvrj.apps@gmail.com>
2026-06-02 22:39:51 -0500

    Update firebaseConfig.js

--- src/scripts/firebaseConfig.js
+++ src/scripts/firebaseConfig.js
@@ -1,10 +1,10 @@
 // Replace all PASTE_* values with your Firebase Web App config from:
 // Firebase Console -> Project settings -> General -> Your apps -> Web app -> SDK setup and configuration.
 export const firebaseConfig = {
-    apiKey: "PASTE_API_KEY",
-    authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-    projectId: "PASTE_PROJECT_ID",
-    storageBucket: "PASTE_STORAGE_BUCKET",
-    messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-    appId: "PASTE_APP_ID",
+    apiKey: "AIzaSyARhAXQ4cb1CPmqT5KhKRP-_6T98PVI5wM",
+  authDomain: "typer-66f6a.firebaseapp.com",
+  projectId: "typer-66f6a",
+  storageBucket: "typer-66f6a.firebasestorage.app",
+  messagingSenderId: "566708669474",
+  appId: "1:566708669474:web:c4a7b53ca890f17d6ece7b"
 };
